4fd85a41b9750b5f5a20390375e305658e512cca
Dmitriy <dima06444@gmail.com>
2023-01-19 11:20:01 +0200

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -17,9 +17,9 @@ function Form() {
   }, []);
 
   useEffect(() => {
-    tg.WebApp.onEvent("mainButtonClicked", onSendData);
+    tg.onEvent("mainButtonClicked", onSendData);
     return () => {
-      tg.WebApp.offEvent("mainButtonClicked", onSendData);
+      tg.offEvent("mainButtonClicked", onSendData);
     };
   }, []);
 
