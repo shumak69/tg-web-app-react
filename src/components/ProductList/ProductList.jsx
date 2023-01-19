d78a93c29e9ffeec7285c537198a8c74f4777a9c
Dmitriy <dima06444@gmail.com>
2023-01-19 16:50:46 +0200

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { useTelegram } from "../../hooks/useTelegram";
 import ProductItem from "../ProductItem/ProductItem";
 import "./productList.css";
