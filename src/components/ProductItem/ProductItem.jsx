df0e2b94680ba9b3db91c5d03bb0cf2539e2770e
Dmitriy <dima06444@gmail.com>
2023-01-19 15:24:57 +0200

    init

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -5,7 +5,7 @@ function ProductItem({ product, className, onAdd }) {
     onAdd(product);
   };
   return (
-    <div className={"product" + className}>
+    <div className={"product " + className}>
       <div className="img"></div>
       <div className="title">{product.title}</div>
       <div className="description">{product.description}</div>
