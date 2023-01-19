ce3c355078891cb640e4252ac1751d40c768f876
Dmitriy <dima06444@gmail.com>
2023-01-19 15:20:14 +0200

    init

---
+++ src/components/ProductItem/ProductItem.jsx
@@ -0,0 +1,24 @@
+import "./productItem.css";
+import Button from "../Button/Button";
+function ProductItem({ product, className, onAdd }) {
+  const onAddHandler = () => {
+    onAdd(product);
+  };
+  return (
+    <div className={"product" + className}>
+      <div className="img"></div>
+      <div className="title">{product.title}</div>
+      <div className="description">{product.description}</div>
+      <div className="price">
+        <span>
+          Стоимость: <b>{product.price}</b>
+        </span>
+      </div>
+      <Button className="add-btn" onClick={onAddHandler}>
+        Добавить в корзину
+      </Button>
+    </div>
+  );
+}
+
+export default ProductItem;
